ba11387282881cba7f92f21cdde0cb07e67112bc
unknown <vighetogithub17@gmail.com>
2025-04-17 16:10:43 +0200

    Clean the telemetry, lot of zeroes samples

--- telemetryReal.py
+++ telemetryReal.py
@@ -3,9 +3,25 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv(r"C:\data\LogMoto\E23_CREMONA_20241023_Turno1\E23_CREMONA_20241023_Turno1.csv")
 
+df['timestamps'] = pd.to_numeric(df['timestamps'], errors='coerce')
+
+fig, axs = plt.subplots(2,2, figsize=(16, 8))
+df['tps_lv'] = pd.to_numeric(df['tps_lv'], errors='coerce')
+axs[0,0].plot(df["timestamps"],df["tps_lv"])
+axs[0,0].set_title('tps_lv')
+axs[0,0].set_ylabel('Tps Value')
+axs[0,0].set_xlabel('Time [ms]')
+
+plt.plot()
+plt.show()
+
+print(df.columns)
+
 fig, axs = plt.subplots(2,2, figsize=(16, 8))
 
-df['timestamps'] = pd.to_numeric(df['timestamps'], errors='coerce')
+df = df[1500000:].reset_index()
+df['brake_a'] = pd.to_numeric(df['brake_a'], errors='coerce')
+df["brake_a"] = df["brake_a"].apply(lambda x: 0 if x < 0 else x)
 
 df['tps_lv'] = pd.to_numeric(df['tps_lv'], errors='coerce')
 axs[0,0].plot(df["timestamps"],df["tps_lv"])
@@ -13,11 +29,10 @@ axs[0,0].set_title('tps_lv')
 axs[0,0].set_ylabel('Tps Value')
 axs[0,0].set_xlabel('Time [ms]')
 
-df['brake_a'] = pd.to_numeric(df['brake_a'], errors='coerce')
-axs[0,1].plot(df["timestamps"],df["brake_a"])
+axs[0,1].plot(df["brake_a"])
 axs[0,1].set_title('brake_a')
 axs[0,1].set_ylabel('m/s')
-axs[0,1].set_xlabel('Time [ms]')
+axs[0,1].set_xlabel('N° Of Sample')
 
 df['INV_Motor_Current'] = pd.to_numeric(df['INV_Motor_Current'], errors='coerce')
 axs[1,0].plot(df["timestamps"],df["INV_Motor_Current"])
